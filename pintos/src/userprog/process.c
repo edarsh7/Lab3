6135684abfde5e8be641f446686cd5a5be829fe9
edarsh7 <edarsh7@gmail.com>
2021-03-05 14:06:47 -0800

    idk

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -95,7 +95,7 @@ push_command(const char *cmdline UNUSED, void **esp)
 
     *esp -= sizeof(char*);
     printf("argv[0]: 0x%08x   x: 0x%08x\n", (unsigned int) *esp , (unsigned int)x);
-    memcpy(*esp, x, sizeof(char*));
+    memcpy(*esp, (char*)x, sizeof(char*));
 
     unsigned int y = (unsigned int)*esp;
     *esp -= sizeof(char**);

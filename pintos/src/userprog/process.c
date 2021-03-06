b8654b5d5d29fef1ef8e0c7b48be6a246e602b32
edarsh7 <edarsh7@gmail.com>
2021-03-05 17:35:15 -0800

    idk

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -160,7 +160,7 @@ process_execute(const char *cmdline)
     // Create a Kernel Thread for the new process
     tid_t tid = thread_create(cmdline, PRI_DEFAULT, start_process, cmdline_copy);
 
-    timer_sleep(200);
+    thread_yield();
     // CSE130 Lab 3 : The "parent" thread immediately returns after creating 
     // the child. To get ANY of the tests passing, you need to synchronise the 
     // activity of the parent and child threads.

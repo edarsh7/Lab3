7cafcad194a38d91f129fc805fb562f02e1efb22
edarsh7 <edarsh7@gmail.com>
2021-03-08 03:06:27 -0800

    idk

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -119,6 +119,7 @@ static void exit_handler(struct intr_frame *f)
   int exitcode;
   umem_read(f->esp + 4, &exitcode, sizeof(exitcode));
   thread_current()->p_stat->exit_code = exitcode;
+  printf("real exit return: %d", exitcode);
   semaphore_up(&thread_current()->p_stat->shared);
 
   sys_exit(exitcode);

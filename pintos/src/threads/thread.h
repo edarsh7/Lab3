d22360d9df3557111171d56eee17b49e3c7e8520
edarsh7 <edarsh7@gmail.com>
2021-03-08 01:02:07 -0800

    idk

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -60,6 +60,17 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+
+struct process_status
+{
+    char * cmdline_cpy;
+    struct list_elem child;
+    int pid;
+    int exit_code;
+    int waited;
+    struct semaphore shared;
+    struct semaphore exec;
+};
 /* 
 // A kernel thread or user process.
 //

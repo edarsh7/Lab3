aa3c0fb5638f46ab8f724f607552aaff315e4a77
edarsh7 <edarsh7@gmail.com>
2021-03-07 22:00:28 -0800

    idk

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -175,9 +175,8 @@ process_execute(const char *cmdline)
 
     // Create a Kernel Thread for the new process
     tid_t tid = thread_create(tok, PRI_DEFAULT, start_process, &p_strct);
-     printf("no");
     semaphore_down(&p_strct.sema);
-    
+printf("no");    
 
     // CSE130 Lab 3 : The "parent" thread immediately returns after creating 
     // the child. To get ANY of the tests passing, you need to synchronise the 
@@ -194,7 +193,6 @@ process_execute(const char *cmdline)
 static void
 start_process(void *cmdline)
 {
-    printf("yues");
     // Initialize interrupt frame and load executable. 
     struct intr_frame pif;
     memset(&pif, 0, sizeof pif);

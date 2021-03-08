47a90fe580f1e811f179b26da7985aaddcfa8235
edarsh7 <edarsh7@gmail.com>
2021-03-08 01:07:24 -0800

    idk

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -161,7 +161,7 @@ process_execute(const char *cmdline)
     semaphore_init(&p_strct.exec, 0);
     semaphore_init(&p_strct.shared, 0);
     p_strct.exit_code = 0;
-    p_strct.waited = 0;
+    p_strct.waited = 10;
     thread_current()->p_stat = &p_strct;
 
     p_strct.cmdline_cpy = palloc_get_page(0);

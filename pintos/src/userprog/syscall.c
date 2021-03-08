bd0b0ad27deaa2f96a64ff9a6b9cee3552cd1c9e
edarsh7 <edarsh7@gmail.com>
2021-03-08 03:08:20 -0800

    idk

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -119,6 +119,7 @@ static void exit_handler(struct intr_frame *f)
   int exitcode;
   printf("huhhhh");
   umem_read(f->esp + 4, &exitcode, sizeof(exitcode));
+  printf("huhhhh");
   thread_current()->p_stat->exit_code = exitcode;
   printf("real exit return: %d", exitcode);
   

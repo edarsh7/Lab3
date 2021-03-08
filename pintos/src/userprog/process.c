d295fbb5bb5b21517af680308e98cac5800b3b49
edarsh7 <edarsh7@gmail.com>
2021-03-08 12:24:19 -0800

    idk

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -250,8 +250,6 @@ start_process(void *cmdline)
     }
 
 
-    
-
     // Start the user process by simulating a return from an
     // interrupt, implemented by intr_exit (in threads/intr-stubs.S).  
     // Because intr_exit takes all of its arguments on the stack in 
@@ -274,26 +272,31 @@ start_process(void *cmdline)
 int
 process_wait(tid_t child_tid UNUSED)
 {
-    /* struct list_elem *e;
+    struct thread *t = thread_current();
+    struct list_elem *e;
     struct process_status *ps = NULL;
-    for(e = list_begin(&thread_current()->children);
-        e != list_end(&thread_current()->children);
-        e = list_next(e))
+    if(!list_empty(&t->children))
     {
-        ps = list_entry(e, struct process_status, child);
-        if(ps->pid == child_tid)
+        for(e = list_begin(&thread_current()->children);
+            e != list_end(&thread_current()->children);
+            e = list_next(e))
         {
-            break;
+            ps = list_entry(e, struct process_status, child);
+            if(ps->pid == child_tid)
+            {
+                break;
+            }
         }
     }
-    if(e == list_end(&thread_current()->children) || ps->waited == 1)
+    if(e == list_end(&thread_current()->children) || ps->waited == 1 || ps == NULL)
         return -1;
     
+    ps->waited = 1;
 
     
     semaphore_down(&ps->shared);
-    printf(" %d ", ps->exit_code); */
-    timer_sleep(100);
+
+
     
     
     return -1;
